ab9f2bff59a79ddd54b6e9f400ad7a6121db4a36
Jairik <jairikmc@gmail.com>
2025-04-28 14:58:30 -0400

    Implementing rough client skeleton
    Might be easier to separate client into client-receiver & client-sender. For now, just trying to get it connected with no encryption or anything.

--- client.py
+++ client.py
@@ -3,4 +3,57 @@
 import socket
 import threading
 import sys
+import config  # Importing to access constant keys/settings
 
+# Connect client to server
+print(f"Client started... connected to {config.SERVER_IP}:{config.SERVER_PORT}")
+
+# Create, connect, and validate UDP socket constant IP and port in config.py
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
+    client_socket.connect((config.SERVER_IP, config.SERVER_PORT))  # connect to server
+    print(f"Successfully created socket to connect to server")
+except socket.error as e:  # Socket creation error, exit
+    print(f"Failed to create and create socket: {e}")
+    print("Check firewall settings, try again later, or try running with sudo.")
+    sys.exit(1)
+    
+# Get & validate the user's username
+username = input("Enter your username: ")
+if not username:
+    print("Username cannot be empty. Exiting...")
+    client_socket.close()
+    sys.exit(1)
+
+# Declare listener for messages from other clients
+def listen_for_messages():
+    while True:
+        try:
+            message, _ = client_socket.recvfrom(1024)  # Receive message from server
+            # Add decryption methods (& signature verification) here
+            print(f"Server: {message.decode('utf-8')}")
+        except Exception as e:
+            print(f"Error receiving message: {e}")
+            break
+
+# Start a thread to listen for incoming messages
+listener_thread = threading.Thread(target=listen_for_messages, daemon=True)
+listener_thread.start()
+
+# Main loop to send messages to the server
+# TODO: when connected, send message that the user has joined the chat
+try:
+    while True:
+        message = input("You: ")
+        if message.lower() == 'exit':
+            print("Exiting chat...")
+            break
+        # Perform encryption methods (& signature) here
+        client_socket.sendto(message.encode('utf-8'), (config.SERVER_IP, config.SERVER_PORT))
+except KeyboardInterrupt:
+    print("\nExiting chat...")
+
+
+# Safely close the threads and socket, and exit the program
+# TODO: Send message through socket to notify exit
+client_socket.close()
--- crypto_utils.py
+++ crypto_utils.py
@@ -2,3 +2,9 @@
 
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305 
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
+
+def packageData():
+    ''' Package and encrypt data for sending '''
+    
+def unpackData():
+    ''' Unpack and decrypt data after receiving '''
